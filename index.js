e8b1401b0b12212f50deca1483a5d7d706db1e76
Erhanero <erhancr7@gmail.com>
2022-01-21 13:25:45 +0200

    Install handlebars

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 
 const app = express();
 
+app.engine("hbs", handlebars({
+    extname: "hbs"
+}));
+app.set("view engine", "hbs");
+
 app.all("/", (req, res) => {
     res.send("Hello World");
-    res.end();
+    res.render("index", { layout: false });
 })
 
 app.listen(5000, () => { console.log("Server is running on port 5000") });
