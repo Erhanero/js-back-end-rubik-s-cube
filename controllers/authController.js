dd0fc85c8319e344786adf10801e3651cd088738
Erhanero <erhancr7@gmail.com>
2022-02-06 11:35:29 +0200

    Add user model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
+const authService = require("../services/authService");
 
 router.get("/login", (req, res) => {
     res.render("loginPage");
 });
+
+router.post("/login", (req, res) => {
+
+});
+
+router.get("/register", (req, res) => {
+
+    res.render("registerPage");
+});
+
+router.post("/register", (req, res) => {
+    const { username, password, repeatPassword } = req.body
+    authService.register(username, password, repeatPassword)
+})
 
 module.exports = router;
---
+++ services/authService.js
@@ -0,0 +1,7 @@
+async function register(username, password, repeatPassword) {
+    console.log(username, password, repeatPassword)
+}
+
+module.exports = {
+    register
+}
